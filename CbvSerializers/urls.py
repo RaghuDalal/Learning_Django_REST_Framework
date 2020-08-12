f4b6a87d6fc8804d022f0c870f487faca6bf6f35
Raghvender <raghvender002@gmail.com>
2020-08-12 13:31:17 +0530

    Defined API URL endpoints

---
+++ CbvSerializers/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from CbvSerializers import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('students', views.StudentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- Core/urls.py
+++ Core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('employees/', include('DRF.urls')),
     path('students/', include('serializers.urls')),
+    path('cbv/', include('CbvSerializers.urls')),
 ]
